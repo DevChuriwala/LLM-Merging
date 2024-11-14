bab894430421c461725dfddde282c56b80319320
Ishan Pendse <ishanppendse@Mac.lan1>
2024-11-13 17:29:31 -0800

    Choosing smaller models in Flan T5
    Signed-off-by: Ishan Pendse <ishanppendse@Mac.lan1>

--- llm_merging/merging/FlanT5Avg.py
+++ llm_merging/merging/FlanT5Avg.py
@@ -13,12 +13,12 @@ class FlanT5Avg(Merges):
         These values are meant to be modified by the user.
         '''
         # Give a list of models to load for the merge 
-        self.list_models = [("lorahub/flan_t5_xl-wiki_qa_Is_This_True_", "30a1ee2f857196c1eb996d854548cc19f45ac642"), 
-                            ("lorahub/flan_t5_xl-kilt_tasks_hotpotqa_complex_question", "27d014366bec1c5333ba2e2fae966b7de3c02df1")]
+        self.list_models = [("TARUNBHATT/flan-t5-small-finetuned-squad", "c252e4c05f5f36e3a339675828b3ac843ca31c92"), 
+                            ("NebulaByte/flan_t5_small_squadv1_lora_extractive_qa_merged", "4d234cecc2d4ba33caf034573b388d62d4b7e26a")]
         
         # Hyperparameters 
-        self.base_model_name = "google/flan-t5-xl"
-        self.base_model_revision_id = "7d6315df2c2fb742f0f5b556879d730926ca9001"
+        self.base_model_name = "google/flan-t5-small"
+        self.base_model_revision_id = "0fc9ddf78a1e988dac52e2dac162b0ede4fd74ab"
         self.is_peft = True
         self.max_seq_len = 512
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
